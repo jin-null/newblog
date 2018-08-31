0841d132f5755f546c0a19ec18cc98ee8ad32afb
jinwei <185425157@qq.com>
2018-08-31 10:45:44 +0800

    项目初始化

---
+++ blog/vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+    baseUrl: 'blog/dist',
+    outputDir: '../public/blog/dist',
+    indexPath: '../../../resources/views/welcome.blade.php',
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <link rel=icon href=/blog/dist/favicon.ico>
+    <title>blog</title>
+    <link href=/blog/dist/css/app.7542f025.css rel=preload as=style>
+    <link href=/blog/dist/js/app.983c0356.js rel=preload as=script>
+    <link href=/blog/dist/js/chunk-vendors.df5f2e07.js rel=preload as=script>
+    <link href=/blog/dist/css/app.7542f025.css rel=stylesheet>
+</head>
+<body>
+<div id=app></div>
+<script src=/blog/dist/js/chunk-vendors.df5f2e07.js></script>
+<script src=/blog/dist/js/app.983c0356.js></script>
+</body>
+</html>
